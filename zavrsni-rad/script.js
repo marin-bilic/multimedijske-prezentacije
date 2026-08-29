2966a839511d8cc207ede2fe849316b9298dd3d5
Marin Bilic <marin.bilic@outlook.com>
2026-08-29 19:23:18 +0200

    dinamične podstranice

--- zavrsni-rad/script.js
+++ zavrsni-rad/script.js
@@ -1,7 +1,7 @@
 const content = document.querySelector(".container");
 const nav = document.querySelector(".nav");
 
-
+// NAVIGATION BAR
 changeNavBg = ()=>{
     oldPos = nav.querySelector(".indicator").getBoundingClientRect().left;
     currentPos = nav.querySelector(".current").getBoundingClientRect().left;
@@ -11,14 +11,29 @@ changeNavBg = ()=>{
     nav.querySelector(".background").style.marginLeft=`calc(${newPos}px - 53.5rem)`;
 
 }
+
+// INCLUDING OTHER HTML
+let changePage = (currentPage)=>{
+    fetch(`./pages/${currentPage}.html`)
+        .then(response => response.text())
+        .then(text => content.innerHTML=text)
+}
+
 if(!nav.querySelector(".current")){
-    nav.querySelector(".logo").classList.add("current");
+    nav.querySelector(".motivacija").classList.add("current");
     changeNavBg();
+    changePage("motivacija");
 }
 
 nav.querySelectorAll("li").forEach((item)=>item.addEventListener("click", (e)=>{
     nav.querySelector(".current").classList.remove("current");
+    changePage(e.target.classList[0])
     e.target.classList.add("current");
     changeNavBg();
 }
 ))
+
+
+
+
+  // Outputs the content of the text file
